44dd135d7af4db372a37426f98da6f40e22694d8
hugo hiroshi kondo <hugo.kondo@jerry.cec.ime.usp.br>
2010-04-20 17:34:38 -0300

    Adição de testes do atletas_controller e correção de outros bugs menores, inclusive a view

---
+++ spec/controllers/atletas_controller_spec.rb
@@ -0,0 +1,44 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require "authlogic/test_case"
+
+describe AtletasController do
+  before do
+    activate_authlogic
+    @treinador = Factory(:treinador)
+    @atleta = Factory(:user,{ :treinador_id => @treinador.id })
+		UserSession.create(@atleta)
+  end
+	describe "user is atleta" do
+	
+		describe "get => show" do
+			it "should assign atleta" do
+				get :show
+				assigns[:atleta].should == Atleta.find_by_user_id(@atleta.id)
+			end
+		end
+		
+		describe "get => showProvas" do
+			# Problemas com add provas
+			it "should assign provas" do
+				pending
+				get :show
+				get :showProvas
+				assigns[:provas].should be_kind_of Prova
+			end
+		end
+		
+		describe "get => edit" do
+			it "should assign treinadores" do
+				get :edit
+				assigns[:treinadores].should == [Treinador.find_by_user_id(@treinador.id)]
+			end
+			it "should assign atleta" do
+				get :edit
+				assigns[:atleta].should == Atleta.find_by_user_id(@atleta.id)
+			end
+		end
+		
+	end
+
+end
+
