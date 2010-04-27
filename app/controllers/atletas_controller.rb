e293afed75de25eeb8f47c596a7f2195f64257a9
hugo hiroshi kondo <hugo.kondo@jerry.cec.ime.usp.br>
2010-04-27 17:41:20 -0300

    Hugo+Omar Teste do atletas_controller.rb + atletas.rb

--- app/controllers/atletas_controller.rb
+++ app/controllers/atletas_controller.rb
@@ -2,7 +2,6 @@ class AtletasController < ApplicationController
   
   before_filter :require_login
 
-  
   active_scaffold :atleta do |config|
     config.label = "Atletas"
     config.actions.exclude :create, :update, :delete, :show
@@ -15,8 +14,7 @@ class AtletasController < ApplicationController
   
   def index
   end
-  
-  
+
   def show
     if user_is_treinador?
       render :active_scaffold => "show"
@@ -24,50 +22,48 @@ class AtletasController < ApplicationController
       @atleta = Atleta.find_by_user_id(current_user.id)
     end
   end
-  
-  
-  def showProvas
-    @provas = @atleta.provas
-  end
-  
-  
+
+#  def showProvas
+#    @provas = @atleta.provas
+#  end
+
   def edit
     @treinadores = Treinador.find(:all)
     @atleta = Atleta.find(current_atleta)
   end
-  
-  
+
   def update
     @atleta = Atleta.find(current_atleta)
     if @atleta.update_atleta(params[:atleta])
-      flash[:notice] = 'Atleta foi atualizado com sucesso.'
-      redirect_to(@atleta) 
+      flash[:notice] = "Atleta foi atualizado com sucesso."
+      redirect_to(@atleta)
     else
-      render :action => "edit" 
+      render "edit"
     end
   end
-  
-  
-  def redirect_testes
-    redirect_to atleta_testes_path(@atleta)
-  end
-  
-  def redirect_treinos
-    redirect_to :controller => "treino", :action => "treino_atleta", :atleta_id => @atleta 
-  end
-  
-  def redirect_provas
-    redirect_to atleta_provas_path(@atleta)
-  end
-  
-  
+
+# Redirects devem ser definidos no routes.rb. não há necessidade de ter estes métodos aqui.
+# Foram mantidos até que os problemas de quebra por utilização sejam resolvidos.
+# Long story short: apague isto assim que ver!
+#  def redirect_testes
+#    redirect_to atleta_testes_path(@atleta)
+#  end
+#  
+#  def redirect_treinos
+#    redirect_to :controller => "treino", :action => "treino_atleta", :atleta_id => @atleta 
+#  end
+#  
+#  def redirect_provas
+#    redirect_to atleta_provas_path(@atleta)
+#  end
+
   #Ranking
   def search_ranking
   end
-  
+
   def ranking
     @distancia = params[:distancia]
-    
+
     #@treinador = Treinador.find_by_user_id(current_user)
     #@atletas = Atleta.find_all_by_treinador_id(@treinador)
     @atletas = Atleta.find(:all)
@@ -77,8 +73,7 @@ class AtletasController < ApplicationController
     end
     @provas
   end
-  
-  
+
   #Seleção dos atletas do treinador atualmente logado
   #Isso é automaticamente chamado por active scaffold
   def conditions_for_collection
@@ -88,31 +83,24 @@ class AtletasController < ApplicationController
       flash[:alert] = "Erro: Você não tem acesso a essa página"
       redirect_to root_path()
     end 
-    
-    #['user_type IN (?)', ['admin', 'sysop']]
-  end
-  
-  protected
-  
-  # Segurança
-  def get_atleta
-    if (user_is_treinador?)
-      @atleta = Atleta.find(params[:id])
-    else      
-      if Atleta.find(params[:id]) == current_atleta
-        @atleta = current_atleta
-      else
-        flash[:alert] = "Erro: Você não tem acesso à página "+request.request_uri
-        redirect_to root_path()
-      end
-    end
-  end
-  
-  def integer?(object)
-    true if Integer(object) rescue false
   end
 
-  private :integer?
-  
-  
+#  protected
+
+#  # Segurança
+#  def get_atleta
+#    if (user_is_treinador?)
+#      @atleta = Atleta.find(params[:id])
+#    else      
+#      if Atleta.find(params[:id]) == current_atleta
+#        @atleta = current_atleta
+#      else
+#        flash[:alert] = "Erro: Você não tem acesso à página "+request.request_uri
+#        redirect_to root_path()
+#      end
+#    end
+#  end
+
+
+
 end
--- app/models/atleta.rb
+++ app/models/atleta.rb
@@ -1,37 +1,4 @@
 class Atleta < ActiveRecord::Base
- 
-  has_one :user
-  
-  attr_accessible :usuario, :nasc, :treinador_id, :altura,:peso, :provas, :participa_provas
-
-  def usuario
-    @usuario = User.find(self.user_id) if(self.user_id)
-  end
-  
-  def nome
-    usuario.name
-  end
-  
-	def update_atleta(atleta)
-		self.nasc = Time.parse(atleta["nasc(3i)"] + "-" + atleta["nasc(2i)"] + "-" + atleta["nasc(1i)"])
-		self.altura = atleta[:altura]
-		self.peso= atleta[:peso]
-		self.treinador_id = atleta[:treinador_id]
-		participa_provas = []
-    if atleta[:participa_provas].nil?
-        atleta[:participa_provas] = []
-    end
-    for participa_prova_string in atleta[:participa_provas] do
-      if participa_prova_string.to_i.to_s == participa_prova_string
-        pp = ParticipaProva.find(participa_prova_string)
-      else 
-        pp = ParticipaProva.new(:atleta_id => self.id, :nome => participa_prova_string)
-      end
-      participa_provas <<  pp
-    end
-    self.participa_provas = participa_provas
-    self.save
-	end
 
   has_one :user
  
@@ -45,4 +12,44 @@ class Atleta < ActiveRecord::Base
 
   has_many :participa_provas
   
+  attr_accessible :usuario, :nasc, :treinador_id, :altura,:peso, :provas, :participa_provas
+  
+  validates_presence_of :treinador_id
+
+  def usuario
+    @usuario = User.find(self.user_id) if(self.user_id)
+  end
+  
+  def nome
+    usuario.name
+  end
+
+	def update_atleta(atleta)
+		self.nasc = Time.parse(atleta["nasc(3i)"] + "-" + atleta["nasc(2i)"] + "-" + atleta["nasc(1i)"])
+		self.altura = atleta[:altura]
+		self.peso= atleta[:peso]
+		self.treinador_id = atleta[:treinador_id]
+		participa_provas = []
+    if atleta[:participa_provas].nil?
+        atleta[:participa_provas] = []
+    end
+
+    for participa_prova_string in atleta[:participa_provas] do
+      if integer?(participa_prova_string)
+        pp = ParticipaProva.find(participa_prova_string)
+      else 
+        pp = ParticipaProva.new(:atleta_id => self.id, :nome => participa_prova_string)
+      end
+      participa_provas <<  pp
+    end
+    self.participa_provas = participa_provas
+    self.save
+	end
+
+	private
+
+  def integer?(object)
+    true if Integer(object) rescue false
+  end
+
 end
