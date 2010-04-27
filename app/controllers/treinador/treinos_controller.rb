42efcbdfc58bd12f2a5ca2881cab9569f1cbc2ce
itai de avila soares <itai.soares@hardy.cec.ime.usp.br>
2010-04-27 17:14:07 -0300

    criacao do crud de treinos para treinador e uso de namespace

--- app/controllers/treinador/treinos_controller.rb
+++ app/controllers/treinador/treinos_controller.rb
@@ -6,4 +6,40 @@ class Treinador::TreinosController < ApplicationController
   def new
     @treino = Atleta.find(params[:atleta_id]).treinos.new
   end
+  
+  def update
+    @atleta = Atleta.find(params[:atleta_id])
+    @treino = @atleta.treinos.find(params[:id])
+    if @treino.update_attributes(params[:treino])
+      redirect_to treinador_atleta_treinos_path(@atleta)
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit
+    @atleta = Atleta.find(params[:atleta_id])
+    @treino = Treino.find(params[:id])
+  end
+  
+  def create
+    @atleta = Atleta.find(params[:atleta_id])
+    @treino = @atleta.treinos.new(params[:treino])
+    if @treino.save
+      redirect_to treinador_atleta_treino_path(@atleta,@treino)
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    @atleta = Atleta.find(params[:atleta_id])  
+    @treino = Treino.find(params[:id])
+    @treino.delete
+    redirect_to treinador_atleta_treinos_path(@atleta)
+  end
+  
+  def show
+    @treino = Treino.find(params[:id])
+  end
 end
--- app/models/treino.rb
+++ app/models/treino.rb
@@ -1,3 +1,4 @@
 class Treino < ActiveRecord::Base
   belongs_to :atleta
+  validates_presence_of :date
 end
