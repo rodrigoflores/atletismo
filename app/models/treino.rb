63f9a50dcb8049240b1a8c1f8e2a048c6b4235b0
hugo hiroshi kondo <hugo.kondo@olhovivo.cec.ime.usp.br>
2010-05-11 16:31:26 -0300

    QA feito

--- app/models/treino.rb
+++ app/models/treino.rb
@@ -2,6 +2,7 @@ class Treino < ActiveRecord::Base
   belongs_to :atleta
   has_many :itens_treino, :class_name => "ItemTreino"
   validates_presence_of :date
+  validates_uniqueness_of :date, :scope => :atleta_id
   
   def self.somatorio_de_cargas(atleta, data_inicio, data_termino)
     @treinos = atleta.treinos.all(:conditions => [ "date > ? AND date < ?", data_inicio, data_termino])
@@ -14,9 +15,9 @@ class Treino < ActiveRecord::Base
     for item_treino in @itens_treino do
       #item_treino.grandeza
       somatorio [item_treino.grandeza] += item_treino.series.to_f * item_treino.repeticao.to_f * item_treino.volume.to_f
-      
     end
     
     return somatorio
   end
+  
 end
