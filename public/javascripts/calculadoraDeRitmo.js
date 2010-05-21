3b632a25a5665d24c3ea539865a3290afebbd287
otavio justiniano miyahara santana <otavio.santana@hardy.cec.ime.usp.br>
2010-05-21 17:49:01 -0300

    mudando de aspas duplas para duas aspas simples - OtOmIt

--- public/javascripts/calculadoraDeRitmo.js
+++ public/javascripts/calculadoraDeRitmo.js
@@ -4,6 +4,6 @@ function calculadoraDeRitmo(minutos, segundos, decimos, distancia, intensidade) 
   ritmo = (decimos/distancia)*((200-intensidade)/100);
   minutos = (ritmo/600).floor();
   segundos = ((ritmo%600)/10).toFixed(0);
-  return "Ritmo: " + (minutos > 0 ? (minutos) + "'" : "") + (segundos > 0 ? (segundos) + "\"": "") + "/km" ;
+  return "Ritmo: " + (minutos > 0 ? (minutos) + "'" : "") + (segundos > 0 ? (segundos) + "''": "") + "/km" ;
   
 }
